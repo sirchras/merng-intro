1cdbe4a361ce8aa39499738f74624f1e3523a4a4
sirchras <cjlynch@hotmail.co.nz>
2021-04-10 15:00:51 +1200

    🗃️ add models, add getPosts query

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const { ApolloServer, gql } = require('apollo-server')
 
+const Post = require('./models/Post')
+
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
   type Query {
-    sayHi: String!
+    getPosts: [Post]
   }
 `
 
 const resolvers = {
   Query: {
-    sayHi: () => 'hello world!'
+    async getPosts () {
+      try {
+        const posts = await Post.find()
+        return posts
+      } catch (err) {
+        throw new Error(err)
+      }
+    }
   }
 }
 
