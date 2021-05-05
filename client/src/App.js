87a71a8dc6287a721af6d5a4dd9b771c522378c0
sirchras <cjlynch@hotmail.co.nz>
2021-05-05 15:45:40 +1200

    :construction: create page components

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
+import 'semantic-ui-css/semantic.min.css'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route exact path='/' component={Home}/>
+      <Route path='/login' component={Login}/>
+      <Route path='/register' component={Register}/>
+    </Router>
   );
 }
 
