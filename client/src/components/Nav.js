dac6d97314b56206ade62a930e91a302f44fe4b0
sirchras <cjlynch@hotmail.co.nz>
2021-05-05 17:38:51 +1200

    :sparkles: add links to nav, highlight to active page

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,27 +1,35 @@
 import React, { useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
 import { Menu } from 'semantic-ui-react'
 
 function Nav() {
-  const [activeItem, setActiveItem] = useState('home')
+  const page = (useLocation().pathname.substring(1) || 'home')
+  const [activeItem, setActiveItem] = useState(page)
   const handleItemClick = (e, { name }) => setActiveItem(name)
 
   return (
-    <Menu pointing secondary>
+    <Menu pointing secondary color='purple'>
       <Menu.Item
         name='home'
         active={activeItem === 'home'}
         onClick={handleItemClick}
+        as={Link}
+        to='/'
       />
       <Menu.Menu position='right'>
         <Menu.Item
           name='login'
           active={activeItem === 'login'}
           onClick={handleItemClick}
+          as={Link}
+          to='/login'
         />
         <Menu.Item
           name='register'
           active={activeItem === 'register'}
           onClick={handleItemClick}
+          as={Link}
+          to='/register'
         />
       </Menu.Menu>
     </Menu>
