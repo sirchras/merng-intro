4eac917a6be005c97ed42fead40cded5bdb56ebd
sirchras <cjlynch@hotmail.co.nz>
2021-05-23 12:16:09 +1200

    :construction: update home page component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import { useQuery } from '@apollo/client'
+import { Feed, Header } from 'semantic-ui-react'
+
+import { FETCH_POSTS_QUERY } from '../api/posts'
+import Post from '../components/Post'
 
 function Home () {
+  const { loading, data } = useQuery(FETCH_POSTS_QUERY)
+  const posts = data?.getPosts
+
   return (
-    <h1>home</h1>
+    <Feed size='large'>
+      <Header as='h1' dividing>
+        Recent Posts
+      </Header>
+
+      {
+        loading
+          ? <h3>Loading posts...</h3>
+          : (
+              posts.map(post => (
+                <Post key={post.id} post={post} />
+              ))
+            )
+      }
+    </Feed>
   )
 }
 
