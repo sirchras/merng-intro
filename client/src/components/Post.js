5b0646f6f06697e1ccb84201f5f38a46b5c788c5
sirchras <cjlynch@hotmail.co.nz>
2021-05-26 15:25:25 +1200

    :construction: add evt handlers, link to post

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,35 +1,46 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 import { DateTime } from 'luxon'
 import { Comment, Icon } from 'semantic-ui-react'
 
 function Post ({ post }) {
-  const { username, createdAt, body, likeCount, commentCount } = post
+  const { id, username, createdAt, body, likeCount, commentCount } = post
   const dt = DateTime.fromISO(createdAt)
+
+  function likePost (evt) {
+    console.log('liked post')
+  }
+
+  function commentPost (evt) {
+    console.log('comment on post')
+  }
 
   return (
     <Comment>
       <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/large/patrick.png' />
       <Comment.Content>
         <Comment.Author as='a'>{ username }</Comment.Author>
-        <Comment.Metadata>
-          <div title={dt.toLocaleString(DateTime.DATETIME_FULL)}>
-            {
-              DateTime.now().diff(dt).as('days') > 2
-                ? dt.toLocaleString(DateTime.DATE_MED)
-                : dt.toRelative()
-            }
-          </div>
+        <Comment.Metadata
+          as={Link}
+          to={`/post/${id}`}
+          title={dt.toLocaleString(DateTime.DATETIME_FULL)}
+        >
+          {
+            DateTime.now().diff(dt).as('days') > 2
+              ? dt.toLocaleString(DateTime.DATE_MED)
+              : dt.toRelative()
+          }
         </Comment.Metadata>
         <Comment.Text>{ body }</Comment.Text>
         <Comment.Actions>
-          <Comment.Action>
+          <Comment.Action onClick={likePost}>
             <Icon name='like' />
             {
               `${likeCount} Like${likeCount !== 1 ? 's' : ''}`
             }
           </Comment.Action>
-          <Comment.Action>
+          <Comment.Action onClick={commentPost}>
             <Icon name='reply' />
             {
               `${commentCount} Comment${commentCount !== 1 ? 's' : ''}`
