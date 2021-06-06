e5b324e7e83f7e5dcbec7961aaee15d33c0384fa
sirchras <cjlynch@hotmail.co.nz>
2021-06-06 16:23:07 +1200

    :sparkles: update login form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,8 +1,73 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
+import { useMutation } from '@apollo/client'
+import { Button, Container, Form, Header, Message } from 'semantic-ui-react'
+
+import { LOGIN_USER } from '../api/auth'
+import useForm from '../hooks/useForm'
 
 function Login () {
+  const history = useHistory()
+
+  const { onChange, onSubmit, form } = useForm(() => loginUser(), {
+    username: '',
+    password: ''
+  })
+
+  const [loginUser, { loading, error }] = useMutation(LOGIN_USER, {
+    update (_, result) {
+      history.push('/')
+    },
+    variables: form
+  })
+  const errors = error?.graphQLErrors[0].extensions.exception.errors
+
   return (
-    <h1>login</h1>
+    <Container text>
+      <Header as='h1' textAlign='center' dividing>
+        Login
+      </Header>
+      <Form
+        onSubmit={onSubmit}
+        className={loading && 'loading'}
+      >
+        <Form.Input
+          label='Username'
+          placeholder='Username'
+          name='username'
+          value={form.username}
+          error={!!errors?.username}
+          onChange={onChange}
+          required
+        />
+        <Form.Input
+          label='Password'
+          placeholder='Password'
+          name='password'
+          type='password'
+          value={form.password}
+          error={!!errors?.password}
+          onChange={onChange}
+          required
+        />
+        {
+          error && (
+            <Message error>
+              <Message.List>
+                {
+                  Object.values(errors).map(errMsg => (
+                    <Message.Item key={errMsg}>{errMsg}</Message.Item>
+                  ))
+                }
+              </Message.List>
+            </Message>
+          )
+        }
+        <Button primary>
+          Login
+        </Button>
+      </Form>
+    </Container>
   )
 }
 
