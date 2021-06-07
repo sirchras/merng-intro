2cccb9b5c877b371330b69660acbadd415df6ce9
sirchras <cjlynch@hotmail.co.nz>
2021-06-07 15:40:10 +1200

    :sparkles: set/get token in localStorage

--- client/src/components/auth/AuthProvider.js
+++ client/src/components/auth/AuthProvider.js
@@ -1,13 +1,33 @@
 import React, { createContext, useState } from 'react'
 import PropTypes from 'prop-types'
+import decode from 'jwt-decode'
+
+function getUser () {
+  const token = localStorage.getItem('token')
+  if (!token) return null
+
+  const decoded = decode(token)
+  if (decoded.exp * 1000 < Date.now()) {
+    localStorage.removeItem('token')
+    return null
+  } else {
+    return decoded
+  }
+}
 
 const AuthContext = createContext()
 
 function AuthProvider ({ children }) {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(getUser())
 
-  const login = (user) => setUser(user)
-  const logout = () => setUser(null)
+  const login = (user) => {
+    localStorage.setItem('token', user.token)
+    setUser(user)
+  }
+  const logout = () => {
+    localStorage.removeItem('token')
+    setUser(null)
+  }
 
   return (
     <AuthContext.Provider value={{ user, login, logout }}>
