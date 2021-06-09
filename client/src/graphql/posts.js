14d42987a165c267ad6e1bc30c3dd6e548911290
sirchras <cjlynch@hotmail.co.nz>
2021-06-09 15:26:48 +1200

    :sparkles: add create post mutation

--- client/src/graphql/posts.js
+++ client/src/graphql/posts.js
@@ -15,4 +15,19 @@ const FETCH_POSTS_QUERY = gql`
   }
 `
 
-export { FETCH_POSTS_QUERY }
+const CREATE_POST_MUTATION = gql`
+  mutation createPost($body: String!) {
+    createPost(body: $body) {
+      id body createdAt username likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id username createdAt body
+      }
+    }
+  }
+`
+
+export { FETCH_POSTS_QUERY, CREATE_POST_MUTATION }
