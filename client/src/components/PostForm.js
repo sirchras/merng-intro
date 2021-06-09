211f124d0d2e169dbc7b02cc548e6895f1b46559
sirchras <cjlynch@hotmail.co.nz>
2021-06-09 17:49:48 +1200

    :sparkles: create new post on form submit

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -2,22 +2,33 @@ import React from 'react'
 import { useMutation } from '@apollo/client'
 import { Button, Form, Segment } from 'semantic-ui-react'
 
-import { CREATE_POST_MUTATION } from '../graphql/posts'
+import { CREATE_POST_MUTATION, FETCH_POSTS_QUERY } from '../graphql/posts'
 import useForm from '../hooks/useForm'
 
 function PostForm () {
-  const { onChange, onSubmit, form } = useForm(() => createPost, { body: '' })
-  // eslint-disable-next-line no-unused-vars
-  const [createPost, { error }] = useMutation(CREATE_POST_MUTATION, {
-    update (_, result) {
-      console.log(result)
+  const { onChange, onSubmit, form } = useForm(() => {
+    createPost()
+    form.body = ''
+  }, { body: '' })
+  const [createPost, { loading }] = useMutation(CREATE_POST_MUTATION, {
+    update (proxy, result) {
+      const data = proxy.readQuery({ query: FETCH_POSTS_QUERY })
+      proxy.writeQuery({
+        query: FETCH_POSTS_QUERY,
+        data: {
+          getPosts: [result.data.createPost, ...data.getPosts]
+        }
+      })
     },
     variables: form
   })
 
   return (
     <Segment basic>
-      <Form onSubmit={onSubmit}>
+      <Form
+        onSubmit={onSubmit}
+        className={loading ? 'loading' : ''}
+      >
         <Form.TextArea
           placeholder={'What\'s up?'}
           name='body'
