91d5e8972396bc3aee3b7df5ebbbd1d136cd9d94
sirchras <cjlynch@hotmail.co.nz>
2021-06-09 15:28:10 +1200

    :sparkles: show new post form is user authd

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useQuery } from '@apollo/client'
 import { Container, Comment, Header, Loader } from 'semantic-ui-react'
 
 import { FETCH_POSTS_QUERY } from '../graphql/posts'
+import { AuthContext } from '../components/auth/AuthProvider'
 import Post from '../components/Post'
+import PostForm from '../components/PostForm'
 
 function Home () {
+  const { user } = useContext(AuthContext)
   const { loading, data } = useQuery(FETCH_POSTS_QUERY)
   const posts = data?.getPosts
 
@@ -14,6 +17,7 @@ function Home () {
       <Header as='h1' textAlign='center' dividing>
         Recent Posts
       </Header>
+      { user && <PostForm /> }
       { loading && <Loader active inline='centered' /> }
       { posts && (
         <Comment.Group size='massive' style={{ maxWidth: '100%' }}>
