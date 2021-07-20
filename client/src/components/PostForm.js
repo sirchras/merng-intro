61f0946fb9eb55d9214ab766d2a30c4ee65b07bb
sirchras <cjlynch@hotmail.co.nz>
2021-07-20 14:15:00 +1200

    add error message to post form

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { useMutation } from '@apollo/client'
-import { Button, Form, Segment } from 'semantic-ui-react'
+import { Button, Form, Message, Segment } from 'semantic-ui-react'
 
 import { CREATE_POST_MUTATION, FETCH_POSTS_QUERY } from '../graphql/posts'
 import useForm from '../hooks/useForm'
 
 function PostForm () {
   const { onChange, onSubmit, form } = useForm(() => {
-    createPost()
+    createPost().catch(console.error)
     form.body = ''
   }, { body: '' })
-  const [createPost, { loading }] = useMutation(CREATE_POST_MUTATION, {
+
+  const [createPost, { loading, error }] = useMutation(CREATE_POST_MUTATION, {
     update (proxy, result) {
       const data = proxy.readQuery({ query: FETCH_POSTS_QUERY })
       proxy.writeQuery({
@@ -22,19 +23,23 @@ function PostForm () {
     },
     variables: form
   })
+  const errMsg = error?.graphQLErrors[0].message
 
   return (
     <Segment basic>
       <Form
         onSubmit={onSubmit}
-        className={loading ? 'loading' : ''}
+        className={`${error ? 'error' : ''}${loading ? ' loading' : ''}`}
       >
         <Form.TextArea
           placeholder={'What\'s up?'}
           name='body'
           onChange={onChange}
           value={form.body}
+          error={!!error}
+          required
         />
+        { error && <Message error list={[errMsg]} /> }
         <Button color='purple'>
           Post
         </Button>
