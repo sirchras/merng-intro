73b5da7646118cd4d4d43aa7dad5c4a22947157d
sirchras <cjlynch@hotmail.co.nz>
2021-07-21 16:02:33 +1200

    wip: update Post page

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useQuery } from '@apollo/client'
+import { Comment, Container, Header, Loader } from 'semantic-ui-react'
 
-function Post (props) {
+import { FETCH_POST_QUERY } from '../graphql/posts'
+import PostItem from '../components/PostItem'
+
+function Post () {
+  const { postId } = useParams()
+  const { loading, data } = useQuery(FETCH_POST_QUERY, {
+    variables: { postId }
+  })
+  const post = data?.getPost
+
   return (
-    <p>hello world!</p>
+    <Container>
+      <Header as='h1' textAlign='center' dividing>
+        Thread
+      </Header>
+      { loading && <Loader active inline='centered' /> }
+      { post && (
+        <Comment.Group size='massive' style={{ maxWidth: '100%' }}>
+          <PostItem key={post.id} post={post} />
+        </Comment.Group>
+      )}
+    </Container>
   )
 }
 
